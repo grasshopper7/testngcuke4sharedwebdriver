21ffdc1101ff883043b7c5eeccb8c9ab805dc029
grasshopper <cool.infy.951@gmail.com>
2019-06-23 00:04:34 +0530

    Update SharedDriver.java

--- testngsharedwebdriver/src/test/java/driver/SharedDriver.java
+++ testngsharedwebdriver/src/test/java/driver/SharedDriver.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SharedDriver {
 
 	public SharedDriver() {
-		System.out.println(Thread.currentThread().getId() + " CONST");
 		if (DriverFactory.getDriver() == null) {
 			System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver.exe");
 			DriverFactory.addDriver(new ChromeDriver());

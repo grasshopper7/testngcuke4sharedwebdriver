1fb1f105cd15e70bdfb836dd9de34664c01fc702
grasshopper7 <grass.hopper.moc@gmail.com>
2019-06-23 22:07:02 +0530

    Added latest cucumber version & webdrivermanager dependency

--- testngsharedwebdriver/src/test/java/driver/DriverFactory.java
+++ testngsharedwebdriver/src/test/java/driver/DriverFactory.java
@@ -8,31 +8,31 @@ import org.openqa.selenium.WebDriver;
 public final class DriverFactory {
 
 	private static ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
-	
-	//To quit the drivers and browsers at the end only. 
+
+	// To quit the drivers and browsers at the end only.
 	private static List<WebDriver> storedDrivers = new ArrayList<>();
-	
+
 	static {
-		Runtime.getRuntime().addShutdownHook(new Thread(){
-		    public void run(){
-		    	storedDrivers.stream().forEach(WebDriver::quit);
-		    }
-		  });
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				storedDrivers.stream().forEach(WebDriver::quit);
+			}
+		});
 	}
-	
+
 	private DriverFactory() {}
-	
+
 	public static WebDriver getDriver() {
 		return drivers.get();
 	}
-	
+
 	public static void addDriver(WebDriver driver) {
 		storedDrivers.add(driver);
 		drivers.set(driver);
 	}
-	
+
 	public static void removeDriver() {
 		storedDrivers.remove(drivers.get());
 		drivers.remove();
-	}	
+	}
 }
--- testngsharedwebdriver/src/test/java/driver/SharedDriver.java
+++ testngsharedwebdriver/src/test/java/driver/SharedDriver.java
@@ -2,11 +2,13 @@ package driver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class SharedDriver {
 
 	public SharedDriver() {
 		if (DriverFactory.getDriver() == null) {
-			System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
 			DriverFactory.addDriver(new ChromeDriver());
 		} 
 	}

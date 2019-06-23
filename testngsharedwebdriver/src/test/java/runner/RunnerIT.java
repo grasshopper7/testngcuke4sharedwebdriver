5c101386567640fa326663a13549adf6418a0e53
grasshopper7 <grass.hopper.moc@gmail.com>
2019-06-24 00:55:02 +0530

    Used default pattern for runner class name

--- testngsharedwebdriver/src/test/java/runner/RunnerIT.java
+++ testngsharedwebdriver/src/test/java/runner/RunnerIT.java
@@ -3,6 +3,6 @@ package runner;
 import cucumber.api.CucumberOptions;
 
 @CucumberOptions(glue = "stepdef", features = "src/test/resources/features/")
-public class Runner extends AbstractTestNGCucumberParallelTests {
+public class RunnerIT extends AbstractTestNGCucumberParallelTests {
 
 }
